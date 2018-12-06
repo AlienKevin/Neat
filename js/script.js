f19c9c86a3906839e76a66ca49e90445990a3fbc
Kevin Li <kevinli020508@gmail.com>
2018-12-06 08:27:19 -0500

    Create PowCalc project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let input = document.getElementById("input");
+let evalBtn = document.getElementById("evalBtn");
+evalBtn.addEventListener("click", function(event){
+    console.log("button clicked!");
+    console.log(input.value);
+});
