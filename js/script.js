8acd2ab1c4a7804348bbba74e36c4c8999cc0f9c
Kevin Li <kevinli020508@gmail.com>
2018-12-08 19:32:45 -0500

    Add popup window and trigger button for creating new custom formulas

--- js/script.js
+++ js/script.js
@@ -11,6 +11,33 @@ let convertToLaTeX = converToLaTexDefault;
 document.addEventListener("DOMContentLoaded", function(event) {
   createNewField();
 })
+// Create Formula Button
+// Get the modal
+const modal = document.getElementById('createFunctionWindow');
+
+// Get the button that opens the modal
+const btn = document.querySelector("#createFormulaBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 //listen for keyboard events in the input box and update result display in output box
 mathField.addEventListener("keyup", function(event) {
   //if key pressed is ENTER, UP Arrow, DOWN Arrow, return immediately
