ceed14c879e0ebfb00ba678aecb7f2ccc79ff2e8
Kevin Li <kevinli020508@gmail.com>
2018-12-12 19:44:53 -0500

    Simplify radToDeg function

--- js/functions.js
+++ js/functions.js
@@ -9,44 +9,15 @@ nerdamer.replaceFunction('sin', function(sin, core) {
   };
 });
 let radToDeg = function(rad) {
-  // when rad begins to be >= 683565291, result is no longer precise
   let pi = Math.PI;
   let smallRad = rad % (2 * pi);
-  switch (smallRad){
-    case 0:
-      return 0;
-    case pi / 6:
-      return 30;
-    case pi / 4:
-      return 45;
-    case pi / 3:
-      return 60;
-    case pi / 2:
-      return 90;
-    case 2 * pi / 3:
-      return 120;
-    case 3 * pi / 4:
-      return 135;
-    case 5 * pi / 6:
-      return 150;
-    case pi:
-      return 180;
-    case 7 * pi / 6:
-      return 210;
-    case 5 * pi / 4:
-      return 225;
-    case 4 * pi / 3:
-      return 240;
-    case 3 * pi / 2:
-      return 270;
-    case 5 * pi / 3:
-      return 300;
-    case 7 * pi / 4:
-      return 315;
-    case 11 * pi / 6:
-      return 330;
-    default:
-      return smallRad / (pi) * 180;
+  let deg = smallRad / (pi) * 180;
+  let errorBound = 1e-6;
+  // adjust floating point imprecisions
+  if (Math.abs(deg - Math.round(deg)) <= errorBound){
+    return Math.round(deg);
+  } else{
+    return deg;
   }
 }
 let testRadToDeg = function(rad){
