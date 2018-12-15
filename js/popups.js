f894c262544cf06042524f0c593893bc4bf0bb09
Kevin Li <kevinli020508@gmail.com>
2018-12-14 19:17:57 -0500

    Change the display of modal inputs' error messages to be inline after the inputs themselves

--- js/popups.js
+++ js/popups.js
@@ -28,31 +28,56 @@ for (let i = 0; i < popupIds.length; i++) {
     }
   }
 }
-let closeWindow = function(modal){ // hide the popup window
+let closeWindow = function(modal) { // hide the popup window
   modal.style.display = "none";
 }
 // setting window
 const settingWindow = document.querySelector("#settingWindow");
 const saveSettingBtn = settingWindow.querySelector("button#saveSettingBtn");
-saveSettingBtn.addEventListener("click", function(){
+saveSettingBtn.addEventListener("click", function() {
   let inputs = settingWindow.querySelectorAll("div.modal-body input");
-  for (let input of inputs){
-    switch (input.name){
+  for (let input of inputs) {
+    switch (input.name) {
       case "decimalPrecision":
-        setPrecision(Number(input.value));
+        setPrecision(input.value, input);
         break;
     }
   }
 });
-let setPrecision = function(value){
-  if (value >= 0 && value <= MAX_PRECISION){
-    precision = value;
-    showPopupMessage("Settings saved!");
-    closeWindow(settingWindow);
-  } else{
-    showPopupMessage("precision value is out of range, should be from 0 to " + MAX_PRECISION);
+let setPrecision = function(value, input) {
+  if (value === "") {
+    showMessage("Please input a number", input);
+  } else {
+    value = Number(value);
+    if (Number.isFinite(value) && Number.isInteger(value)) { // input is a number
+      // console.log("value: " + value);
+      if (value >= 0 && value <= MAX_PRECISION) {
+        precision = value;
+        showMessage("Settings saved!", saveSettingBtn);
+        window.setTimeout(function(){
+          removeOldMessage();
+          closeWindow(settingWindow);
+        }, 500);
+      } else {
+        showMessage("Precision value is out of range, should be from 0 to " + MAX_PRECISION, input);
+      }
+    } else{
+      showMessage("Input should be a valid integer", input);
+    }
   }
 }
-let showPopupMessage = function(message){
-  window.alert(message);
+let showMessage = function(message, place) {
+  removeOldMessage();
+  let msg = document.createElement("span");
+  msg.style.paddingLeft = "10px";
+  msg.className = "message";
+  msg.innerHTML = message;
+  place.parentNode.insertBefore(msg, place.nextSibling);
+}
+
+let removeOldMessage = function(){
+  let oldMsg = document.querySelector("div.modal span.message");
+  if (oldMsg !== null) {
+    oldMsg.remove(); // remove old message
+  }
 }
